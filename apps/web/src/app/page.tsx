e74023e9e69350f85b1e94fa9f8d9a873dbdc8d3
isaf123 <f.isa283@gmail.com>
2024-04-26 13:00:42 +0700

    feat: install shadcn-ui

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,11 +1,17 @@
-import { Footer } from "@/components/Footer";
-import { Header } from "@/components/Header";
+import { Footer } from '@/components/Footer';
+import { Header } from '@/components/Header';
+import { Button } from '@/components/ui/button';
+import { ComboboxDemo } from '@/components/ui/combobox';
 
 export default function Home() {
   return (
     <main>
       <Header />
+      <Button variant="outline" type="button" className="bg-red-600">
+        Click
+      </Button>
+      <ComboboxDemo></ComboboxDemo>
       <Footer />
     </main>
-  )
+  );
 }
--- apps/web/src/components/Header.tsx
+++ apps/web/src/components/Header.tsx
@@ -1,3 +1,3 @@
 export const Header = () => {
-  return <div className="text-blue-300">Header</div>;
+  return <div className="text-blue-300 bg-black w-full h-[40px]">Header</div>;
 };
