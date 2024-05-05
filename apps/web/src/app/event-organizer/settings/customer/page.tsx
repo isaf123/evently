095d33860ecc58ce9c71c19a11c02effc903f2f6
isaf123 <f.isa283@gmail.com>
2024-05-05 20:26:09 +0700

    feat: update create event

--- apps/web/src/app/event-organizer/settings/customer/page.tsx
+++ apps/web/src/app/event-organizer/settings/customer/page.tsx
@@ -1,8 +1,19 @@
+import Header from '@/components/SidebarEO/header'
+import HeaderMobile from '@/components/SidebarEO/header-mobile'
+import SideNav from '@/components/SidebarEO/side-nav'
 import React from 'react'
 
 const CustomerEOPage = () => {
     return (
-        <div>CustomerEOPage</div>
+        <div className='flex'>
+            <SideNav />
+            <div className='flex-1'>
+                <Header />
+                <HeaderMobile />
+                <div className='flex flex-col container px-[180px] my-[10px] mx-auto'>Customer EO</div>
+            </div>
+        </div >
+
     )
 }
 
