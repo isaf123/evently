3362f8cae897347d6d6480e8f358b81524458392
ronaldj220 <ronjuliostagjhs21@gmail.com>
2024-05-06 13:11:18 +0700

    feat: add username in header customers

--- apps/web/src/components/Header.tsx
+++ apps/web/src/components/Header.tsx
@@ -1,56 +1,75 @@
-'use client';
+'use client'
 import { Button } from '@/components/ui/button';
-import Cookies from 'js-cookie';
+import { setLogoutAction, selectUserRole } from '@/lib/features/userSlice'; // Perhatikan bahwa saya mengubah ini untuk mengambil fungsi logout dan selector role
+import { useAppDispatch, useAppSelector } from '@/lib/hooks';
 import { Search } from 'lucide-react';
 import Link from 'next/link';
+import React from 'react';
 
 export const Header = () => {
-  const token = Cookies.get('token Cust');
-  const tokenEO = Cookies.get('token EO');
+
+
+  const dispatch = useAppDispatch();
+
+  const username = useAppSelector((state) => state.userSlice.username); // Mengambil username dari Redux store
+
+  const role = useAppSelector(selectUserRole); // Mengambil role pengguna dari Redux store
+
+  const handleLogout = () => {
+    // Dispatch action logout
+    dispatch(setLogoutAction());
+  };
+
+  if (role === 'eo') {
+    return null; // Jika role pengguna adalah "eo", kembalikan null untuk menyembunyikan header
+  }
 
   return (
-    <div>
-      {tokenEO ? (
-        <div className="text-white bg-[#333A73] w-full h-[80px]">
-          <div className="w-full h-full flex items-center justify-between mr-10">
-            <Link className="ml-20 font-extrabold text-[20px]" href={'/'}>
-              EVENTLY
-            </Link>
-            <div className="flex items-center">
-              <input
-                type="text"
-                className="h-[34px] w-[420px] rounded-l-md"
-                placeholder="Search Your Event"
-              />
-              <button className="bg-blue-500 h-[34px] w-[80px] rounded-r-md">
-                <Search className="w-[80px]" />
-              </button>
-            </div>
-            {!token ? (
-              <div className="flex gap-5 mr-20 items-center">
-                <Link className="mr-5 font-medium capitalize" href={'/explore'}>
-                  {' '}
-                  explore{' '}
-                </Link>
-                <Link className="w-fit h-fit" href={'/signup'}>
-                  <Button className="bg-white text-[#333A73] font-bold">
-                    Sign Up
-                  </Button>
-                </Link>
-                <Link className="w-fit h-fit" href={'/signin'}>
-                  <Button className=" border border-lg border-white">
-                    Sign In
-                  </Button>
-                </Link>
-              </div>
-            ) : (
-              <div className="">Oke</div>
-            )}{' '}
-          </div>
+    <div className='text-white bg-[#333A73] w-full h-[80px]'>
+      <div className="w-full h-full flex items-center justify-between mr-10">
+        <Link className="ml-20 font-extrabold text-[20px]" href={'/'}>
+          EVENTLY
+        </Link>
+
+        <div className="flex items-center">
+          <input
+            type="text"
+            className="h-[34px] w-[420px] rounded-l-md"
+            placeholder="Search Your Event"
+          />
+          <button className="bg-blue-500 h-[34px] w-[80px] rounded-r-md">
+            <Search className="w-[80px]" />
+          </button>
         </div>
-      ) : (
-        <div className=""></div>
-      )}
+
+        <div className="flex gap-5 mr-20 items-center">
+          <Link className="mr-5 font-medium capitalize" href={'/explore'}>
+            {' '}
+            explore{' '}
+          </Link>
+          {role === 'customers' && ( // Hanya menampilkan tombol Sign Out jika pengguna adalah customers
+            <Button onClick={handleLogout} className="border border-lg border-white">
+              Sign Out
+            </Button>
+          )}
+          {username ? ( // Jika ada username, tampilkan username
+            <span className="mr-5 font-medium capitalize">{username}</span>
+          ) : (
+            <Link className="w-fit h-fit" href={'/signup'}>
+              <Button className="bg-white text-[#333A73] font-bold">
+                Sign Up
+              </Button>
+            </Link>
+          )}
+          {!username && ( // Jika tidak ada username, tampilkan tombol Sign In
+            <Link className="w-fit h-fit" href={'/signin'}>
+              <Button className=" border border-lg border-white">Sign In</Button>
+            </Link>
+          )}
+        </div>
+      </div>
     </div>
   );
 };
+
+export default Header;
