abc7b8fba68f315bdfcd174865fa1cb6b532aec3
ronaldj220 <65749850+ronaldj220@users.noreply.github.com>
2024-05-06 10:06:16 +0700

    Add middleware nextjs fe (#15)
    * feat: sidebar EO Page
    
    * feat: testing back end
    
    * feat: add database event min pro pwd
    
    * fix: update schema prisma]
    
    * feat: send email in nodemailer
    
    * feat: add register Users back end
    
    * feat: forgot password and login back end
    
    * feat: adding login at back end
    
    * feat: adding register and login with back end and front end
    
    * feat: implement login and token in front end and back end
    
    * feat: validation registration front end
    
    * feat: adding master event in EO
    
    * fix: update status if logn 3 times suspend account
    
    * feat: add middleware nextjs
    
    * fix: updated middleware nextjs in eo and customers

--- apps/web/src/middleware.ts
+++ apps/web/src/middleware.ts
@@ -8,5 +8,5 @@ export function middleware(request: NextRequest) {
 
 // See "Matching Paths" below to learn more
 export const config = {
-    matcher: ['/event-organizer/dashboard', '/'],
+    matcher: ['/event-organizer/:path*', '/signup', '/'],
 }
