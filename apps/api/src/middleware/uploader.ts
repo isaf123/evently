4a1a59abf3a6cbd17fb78b563a6f8a3acff3a172
isaf123 <f.isa283@gmail.com>
2024-05-08 16:14:58 +0700

    feat: update event-pic

---
+++ apps/api/src/middleware/uploader.ts
@@ -0,0 +1,36 @@
+import { Request } from 'express';
+import multer from 'multer';
+import { join } from 'path'; // untuk merge file location
+
+export const uploader = (dirName?: string, filePrefix?: string) => {
+  const defaultDir = join(__dirname, '../../public'); //define directory utama
+
+  const configStore = multer.diskStorage({
+    destination: (
+      req: Request,
+      file: Express.Multer.File,
+      callback: (error: Error | null, destination: string) => void,
+    ) => {
+      const fileDestination = dirName ? defaultDir + dirName : defaultDir;
+      console.log('Destination :', fileDestination);
+      callback(null, fileDestination);
+    },
+    filename: (
+      req: Request,
+      file: Express.Multer.File,
+      callback: (error: Error | null, filename: string) => void,
+    ) => {
+      const existName = file.originalname.split('.');
+      console.log('Original Filename', existName);
+      const extension = existName[existName.length - 1];
+      if (filePrefix) {
+        const newName = filePrefix + Date.now() + '.' + extension;
+        callback(null, newName);
+      } else {
+        callback(null, file.originalname);
+      }
+    },
+  });
+
+  return multer({ storage: configStore });
+};
