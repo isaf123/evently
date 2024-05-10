eff7fa72606143374d844d419d18a265790f0839
isaf123 <137991239+isaf123@users.noreply.github.com>
2024-05-10 10:31:25 +0700

    feat: update event pic router (#38)

---
+++ apps/api/src/routers/EO/eventpic.router.ts
@@ -0,0 +1,26 @@
+import { Router } from 'express';
+import { EventPicController } from '@/controllers/customer/eventpic.controller';
+import { uploader } from '@/middleware/uploader';
+
+export class EventPicRouter {
+  private router: Router;
+  private eventPicController: EventPicController;
+
+  constructor() {
+    this.router = Router();
+    this.eventPicController = new EventPicController();
+    this.initializeRoute();
+  }
+
+  private initializeRoute(): void {
+    this.router.patch(
+      '/photo/:id',
+      uploader('/eventpic', 'EVENTPIC').single('eventPic'),
+      this.eventPicController.eventImg,
+    );
+  }
+
+  getRouter(): Router {
+    return this.router;
+  }
+}
