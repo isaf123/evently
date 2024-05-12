723ecb00da224c684b304e7e7fbc582f1c4d75e1
isaf123 <137991239+isaf123@users.noreply.github.com>
2024-05-12 17:53:39 +0700

    feat: update create validation (#46)

--- apps/api/src/services/EO/event/createEvent.ts
+++ apps/api/src/services/EO/event/createEvent.ts
@@ -31,13 +31,13 @@ export const validationEvent = async (
       throw 'invalid price';
     }
 
-    const { start_date, end_date } = eventData;
-    if (
-      start_date.getTime() > end_date.getTime() ||
-      start_date.getTime() < new Date().getTime()
-    ) {
-      throw 'invalid date';
-    }
+    // const { start_date, end_date } = eventData;
+    // if (
+    //   start_date.getTime() > end_date.getTime() ||
+    //   start_date.getTime() < new Date().getTime()
+    // ) {
+    //   throw 'invalid date';
+    // }
 
     return '';
   } catch (error) {
@@ -52,26 +52,26 @@ export const createEvent = async (
   usersId: number,
 ) => {
   try {
-    // console.log('dapet data :', eventData);
-    // console.log('dapet pic :', pic);
-    // console.log('dapet user :', usersId);
-    // const newEvent = await prisma.masterEvent.create({
-    //   data: {
-    //     title: eventData.title,
-    //     description: eventData.description,
-    //     category: eventData.category,
-    //     available_seat: Number(eventData.available_seat),
-    //     event_type: eventData.event_type,
-    //     price: Number(eventData.price),
-    //     location: eventData.location,
-    //     address: eventData.address,
-    //     start_date: eventData.start_date,
-    //     end_date: eventData.end_date,
-    //     flyer_event: pic,
-    //     usersId,
-    //   },
-    // });
-    // return newEvent;
+    console.log('dapet data :', eventData);
+    console.log('dapet pic :', pic);
+    console.log('dapet user :', usersId);
+    const newEvent = await prisma.masterEvent.create({
+      data: {
+        title: eventData.title,
+        description: eventData.description,
+        category: eventData.category,
+        available_seat: Number(eventData.available_seat),
+        event_type: eventData.event_type,
+        price: Number(eventData.price),
+        location: eventData.location,
+        address: eventData.address,
+        start_date: eventData.start_date,
+        end_date: eventData.end_date,
+        flyer_event: pic,
+        usersId,
+      },
+    });
+    return newEvent;
   } catch (error) {
     return error;
   }
