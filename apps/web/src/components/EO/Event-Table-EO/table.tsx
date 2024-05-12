976dac42c1c96f80217d12dcf41fba2bfe38b45d
ronaldj220 <ronjuliostagjhs21@gmail.com>
2024-05-12 11:40:24 +0700

    feat: updating list of events based on userid

--- apps/web/src/components/EO/Event-Table-EO/table.tsx
+++ apps/web/src/components/EO/Event-Table-EO/table.tsx
@@ -1,11 +1,71 @@
-import { Table, TableHead, TableHeader, TableRow } from '@/components/ui/table'
-import React from 'react'
+'use client'
+import { showMessage } from '@/components/Alert/Toast';
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table';
+import axios from 'axios';
+import * as React from 'react';
+import { formatDate } from '../../../lib/EO/formatDate';
+import { generateEventCode } from '@/lib/EO/generateEventCode';
+import Cookies from 'js-cookie';
+
+interface EventData {
+    id: number;
+    flyer_event: string | null;
+    title: string;
+    start_date: string;
+    end_date: string;
+    description: string;
+    category: string;
+    available_seat: number;
+    event_type: string;
+    price: number;
+    location: string;
+    usersId: number;
+    address: string;
+    user_id: {
+        name: string;
+    };
+    eventCode: any
+}
 
 const TableEventEO = () => {
+    const [dataEvent, setDataEvent] = React.useState<EventData[]>([])
+    const getEvents = async () => {
+        try {
+            const cookies = Cookies.get('Token EO')
+            // console.log(cookies);
+
+            const response = await axios.get(`${process.env.NEXT_PUBLIC_BASE_API_URL}event-organizer/event`, {
+                headers: {
+                    Authorization: `Bearer ${cookies}`
+                }
+            })
+            console.log('data event', response);
+
+
+            const events = response.data.map((event: EventData, index: number) => ({
+                ...event,
+                eventCode: generateEventCode(index + 1) // Menambahkan properti eventCode ke setiap event
+            }));
+            setDataEvent(events)
+        } catch (error: any) {
+            if (error.response) {
+                showMessage(error.response.data.error.message, 'error');
+            } else {
+                showMessage(error, 'error');
+            }
+        }
+    }
+    React.useEffect(() => {
+        getEvents()
+    }, []);
+
+    // Handler untuk memperbarui nilai query saat input berubah
+
     return (
-        <div className='bg-red-300 w-[950px] text-justify md: ml-[300px]'>
-            <Table>
-                <TableHeader className=''>
+        <div>
+
+            <Table className='w-full text-sm text-left text-gray-500'>
+                <TableHeader className='text-sm text-gray-700 uppercase bg-gray-50'>
                     <TableRow>
                         <TableHead className='px-6 py-3 hidden md:table-cell'>Event Code</TableHead>
                         <TableHead className='px-6 py-3'>Event Name</TableHead>
@@ -26,7 +86,7 @@ const TableEventEO = () => {
                     ))}
                 </TableBody>
             </Table>
-        </div>
+        </div >
     );
 };
 
